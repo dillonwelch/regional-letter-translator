f249b8d754e0b7bd1ec2c96c3fe0ecc047d25166
Dillon Welch <daw0328@gmail.com>
2018-06-29 21:25:38 -0700

    Remove document.ready

--- application.js
+++ application.js
@@ -1,12 +1,10 @@
-$('document').ready(function() {
-  $('#input').bind('input propertychange', function() {
-    letters = $(this).val().split('').map(function(letter) {
-      if(letter.match(/[a-z]/i)) {
-        return ':regional_indicator_' + letter + ':';
-      } else {
-        return letter
-      }
-    });
-    $('#output').val(letters.join(' '));
-  })
-});
+$('#input').bind('input propertychange', function() {
+  letters = $(this).val().split('').map(function(letter) {
+    if(letter.match(/[a-z]/i)) {
+      return ':regional_indicator_' + letter + ':';
+    } else {
+      return letter
+    }
+  });
+  $('#output').val(letters.join(' '));
+})
