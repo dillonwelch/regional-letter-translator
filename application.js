9d69a722cf487830ea16e593c48d356e92792505
Dillon Welch <daw0328@gmail.com>
2018-06-29 21:19:24 -0700

    Filter all non-letter values

--- application.js
+++ application.js
@@ -2,10 +2,10 @@ $('document').ready(function() {
   $('#input').bind('input propertychange', function() {
     area = $(this)
     letters = area.val().split('').map(function(letter) {
-      if(letter === ' ') {
-        return ' ';
-      } else {
+      if(letter.match(/[a-z]/i)) {
         return ':regional_indicator_' + letter + ':';
+      } else {
+        return letter
       }
     });
     $('#output').val(letters.join(' '));
