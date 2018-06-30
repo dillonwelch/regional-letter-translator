725e2935b95720a40ec075754f3f8fc1f9808175
Dillon Welch <daw0328@gmail.com>
2018-06-30 13:36:06 -0700

    Add support for capital letters

--- application.js
+++ application.js
@@ -1,7 +1,7 @@
 document.getElementById('input').addEventListener('input', function() {
   letters = this.value.split('').map(function(letter) {
     if(letter.match(/[a-z]/i)) {
-      return ':regional_indicator_' + letter + ':';
+      return ':regional_indicator_' + letter.toLowerCase() + ':';
     } else {
       return letter
     }
